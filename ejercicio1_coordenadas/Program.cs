1465758d1ae65f1230a05d7beb4ffc2132661919
alejandro lemos gomez <alejandrolemosgomez@hotmail.com>
2024-08-30 17:35:31 -0500

    agregando otras entradas al codigo

--- ejercicio1_coordenadas/Program.cs
+++ ejercicio1_coordenadas/Program.cs
@@ -2,10 +2,21 @@
 {
     private static void Main(string[] args)
     {
-         // declarar las variables; para este ejerciocio son 4:
-          double x1,x2,y1,y2;
+        // declarar las variables; para este ejerciocio son 4:
+        double x1, x2, y1, y2, d;
         //vamos a inicializar lass variable,pidiendolas por consola
         Console.WriteLine("por favor ingrese el valor de x1:");
-       x1= Convert.ToDouble(Console.ReadLine());// convertiendo x1 de string a double
+        x1 = Convert.ToDouble(Console.ReadLine());// convertiendo x1 de string a double
+        Console.WriteLine("por favor ingrese el valor de x2:");
+        x2 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("por favor ingrese el valor de y1:");
+        y1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("por favor ingrese el valor de y2:");
+        y2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+
     }
 }
