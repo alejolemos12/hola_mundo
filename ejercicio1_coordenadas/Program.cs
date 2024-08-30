446379ea35e7e609587c655366a6fba6ec9d98d2
alejandro lemos gomez <alejandrolemosgomez@hotmail.com>
2024-08-30 12:37:40 -0500

    guardamos avances de mi codigo

--- ejercicio1_coordenadas/Program.cs
+++ ejercicio1_coordenadas/Program.cs
@@ -2,6 +2,10 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+         // declarar las variables; para este ejerciocio son 4:
+          double x1,x2,y1,y2;
+        //vamos a inicializar lass variable,pidiendolas por consola
+        Console.WriteLine("por favor ingrese el valor de x1:");
+       x1= Convert.ToDouble(Console.ReadLine());// convertiendo x1 de string a double
     }
 }
