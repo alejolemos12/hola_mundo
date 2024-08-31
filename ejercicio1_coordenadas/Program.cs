7be2a063e067dc33c45b88c0da52b7cf0563a7e6
alejandro lemos gomez <alejandrolemosgomez@hotmail.com>
2024-08-30 21:31:29 -0500

    se imprime el resultado del ejercicio

--- ejercicio1_coordenadas/Program.cs
+++ ejercicio1_coordenadas/Program.cs
@@ -7,15 +7,22 @@
         //vamos a inicializar lass variable,pidiendolas por consola
         Console.WriteLine("por favor ingrese el valor de x1:");
         x1 = Convert.ToDouble(Console.ReadLine());// convertiendo x1 de string a double
-        Console.WriteLine("por favor ingrese el valor de x2:");
+        Console.WriteLine("por favor ingrese el valor de x2:");//console clase para invocar el metodo
         x2 = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("por favor ingrese el valor de y1:");
         y1 = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("por favor ingrese el valor de y2:");
         y2 = Convert.ToDouble(Console.ReadLine());
 
+        // proceso--> caculo de la variable b
+        double x = (x2 - x1) * (x2 - x1);
+        double y = (y2 - y1) * (y2 - x1);
 
-
+        double suma = x + y;
+        // clase match tiene muchos metodos, aqui estamos elevando al cuadrado los datos que hay en suma
+        d=Convert.ToDouble(Math.Sqrt(suma));
+        // forma de concatenar : interpolacion
+        Console.WriteLine($"la distancia entre los puntos ingresados es:{d}");
 
 
     }
