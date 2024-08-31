ccd4ee39959e6208f80614f837bf296412007f88
alejandro lemos gomez <alejandrolemosgomez@hotmail.com>
2024-08-31 17:31:16 -0500

    linea de codigo comentada

--- ejercicio1_coordenadas/Program.cs
+++ ejercicio1_coordenadas/Program.cs
@@ -1,20 +1,21 @@
 ﻿public class Program
 {
-    private static void Main(string[] args)
+       private static void Main(string[] args)
     {
         // declarar las variables; para este ejerciocio son 4:
         double x1, x2, y1, y2, d;
         //vamos a inicializar lass variable,pidiendolas por consola
         Console.WriteLine("por favor ingrese el valor de x1:");
+        //console clase para invocar el metodo
         x1 = Convert.ToDouble(Console.ReadLine());// convertiendo x1 de string a double
-        Console.WriteLine("por favor ingrese el valor de x2:");//console clase para invocar el metodo
+        Console.WriteLine("por favor ingrese el valor de x2:");
         x2 = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("por favor ingrese el valor de y1:");
         y1 = Convert.ToDouble(Console.ReadLine());
         Console.WriteLine("por favor ingrese el valor de y2:");
         y2 = Convert.ToDouble(Console.ReadLine());
 
-        // proceso--> caculo de la variable b
+        // proceso--> caculo de la variable b, que hace referencia a los puntos
         double x = (x2 - x1) * (x2 - x1);
         double y = (y2 - y1) * (y2 - x1);
 
