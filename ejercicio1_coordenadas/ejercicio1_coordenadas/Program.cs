d352500eccc60646b356cb4266d05950e570383f
alejandro lemos gomez <alejandrolemosgomez@hotmail.com>
2024-09-12 16:27:49 -0500

    ejercicio 1, cambios realizados

--- ejercicio1_coordenadas/ejercicio1_coordenadas/Program.cs
+++ ejercicio1_coordenadas/ejercicio1_coordenadas/Program.cs
@@ -1,68 +1,67 @@
 ﻿public class Program
 {
+    //Significan que es para comentar en línea:
+
+    /* Comentarios en bloque
+     * fasgvadsgbasdbn
+     * dgbasbsdrnr
+     * sdfn srmnrtsedt
+     * sfnsfmnsfmndfs
+     * sfmnsfmn
+     */
+
     private static void Main(string[] args)
     {
-        //Arreglar identación Ctrl + K, Ctrl + D
-        //2.Lea la longitud de tres segmentos y que muestre si se puede construir un triángulo con estos segmentos.
+        //Lea las coordenadas de dos puntos y que muestre la distancia entre los dos puntos. 
 
-        //Delcaración de los inputs
-        double seg1, seg2, seg3;
-        bool isTriangle;
-        string msj;
+        //Datos de Entrada
+        //Declarar las variables, en este caso son 4:
+        double x1, x2, y1, y2, d;
 
-        //Pedir los datos - input:
-        Console.Write("Ingrese el primer segmento: ");
-        seg1 = Convert.ToDouble(Console.ReadLine());
+        //Vamos a inicializar mis variables pidiendoles sus valores por pantalla:
+        Console.Write("Por favor ingrese el valor de x1: "); //Esta es la forma de escribir en pantalla
+        x1 = Convert.ToDouble(Console.ReadLine()); //Esta es la forma de leer en pantalla
 
-        Console.Write("Ingrese el segundo segmento: ");
-        seg2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Por favor ingrese el valor de y1: ");
+        y1 = Convert.ToDouble(Console.ReadLine());
 
-        Console.Write("Ingrese el tercer segmento: ");
-        seg3 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Por favor ingrese el valor de x2: ");
+        x2 = Convert.ToDouble(Console.ReadLine());
 
-        //Proceso - Validación
-        //Validación en cascada
-        if ((seg1 + seg2) > seg3)
-        {
-            if ((seg1 + seg3) > seg2)
-            {
-                if ((seg2 + seg3) > seg1)
-                {
-                    isTriangle = true;
-                }
-                else
-                {
-                    isTriangle = false;
-                }
-            }
-            else
-            {
-                isTriangle = false;
-            }
-        }
-        else
-        {
-            isTriangle = false;
-        }
+        Console.Write("Por favor ingrese el valor de y2: ");
+        y2 = Convert.ToDouble(Console.ReadLine());
 
-        //Validación en una sola línea
-        if (((seg1 + seg2) > seg3) && ((seg1 + seg3) > seg2) && ((seg2 + seg3) > seg1))
-        {
-            isTriangle = true;
-        }
-        else
-        {
-            isTriangle = false;
-        }
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
 
-        //Mostrar mensaje - output
-        if (!isTriangle)
-        {
-            Console.Write("No se puede construir el triángulo");
-        }
-        else
-        {
-            Console.Write("Se puede construir el triángulo");
-        }
+        //Proceso - cálculo de la variable "d";
+        double x = (x2 - x1) * (x2 - x1);
+        double y = (y2 - y1) * (y2 - y1);
+
+        double suma = x + y;
+
+        //La clase Math tiene muchos métodos, entre ellos el método Sqrt, este señor recibe un parámetro dentro de sus ( ), ese parámetro es el número que yo le mando al método para que me saque la Raíz Cuarada!
+        d = Convert.ToDouble(Math.Sqrt(suma));
+
+        //Forma más primitiva de concatenar texto a través del símbolo "+"
+        Console.WriteLine("Concatenar con +");
+        Console.Write("La distancia entre los pares ordenados (" + x1 + "," + y1 + ") y (" + x2 + "," + y2 + ") es: " + d);
+
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+
+        //Forma de concatenar a través del símbolo "$" se llama Interpolación:
+        Console.WriteLine("Concatenar con $");
+        Console.Write($"La distancia entre los pares ordenados ({x1},{y1}) y ({x2},{y2}) es: {d}");
+
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
+
+        Console.WriteLine("Concatenar con String.Format");
+        //Forma de concatenar con la clase String.Format:
+        Console.Write(String.Format("La distancia entre los pares ordenados ({0},{1}) y ({2},{3}) es: {4}", x1, y1, x2, y2, d));
+
+        Console.WriteLine(" ");
+        Console.WriteLine(" ");
     }
 }
