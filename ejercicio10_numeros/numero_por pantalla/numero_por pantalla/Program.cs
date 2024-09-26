7b950cbbe74fbe16fcce894b4eaa6b29e0d30829
alejandro lemos gomez <alejandrolemosgomez@hotmail.com>
2024-09-25 21:35:09 -0500

    ejercicio,realizado

---
+++ ejercicio10_numeros/numero_por pantalla/numero_por pantalla/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        int contador = 0;
+        int num;
+        bool bandera = true;
+        while (bandera)
+        {
+            Console.WriteLine("ingrese un numero:");
+            num = Convert.ToInt32(Console.ReadLine());
+            if (num >= 0)
+            {
+                contador++;
+            }
+            else
+            {
+                bandera = false;
+            }
+        }
+        Console.WriteLine($"el total de numeros positivos ingresados es de:{contador}");
+    }
+}
